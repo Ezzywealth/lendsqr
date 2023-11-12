fb9dcdd801995d9a0f8c8970c5e92ea1387d09c9
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-11-12 21:51:13 +0100

    Install dependencies for jest and add jest and babel config file

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
-export default {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'jsdom',
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	moduleNameMapper: {
+		'\\.(css|scss)$': 'identity-obj-proxy',
+	},
+	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
